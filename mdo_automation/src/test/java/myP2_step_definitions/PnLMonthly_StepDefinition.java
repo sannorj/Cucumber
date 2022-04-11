f349bf2f2194270617095daad45a821da7fd700b
sannorj-mdosl <89373152+sannorj-mdosl@users.noreply.github.com>
2022-04-11 08:56:33 +0530

    P&l monthly Calculations and incorporate with review comments

--- mdo_automation/src/test/java/myP2_step_definitions/PnLMonthly_StepDefinition.java
+++ mdo_automation/src/test/java/myP2_step_definitions/PnLMonthly_StepDefinition.java
@@ -20,13 +20,78 @@ public class PnLMonthly_StepDefinition {
 
 	
 	@When("select the Group ,Propery, date,View and Click on GO button")
-	public void select_the_group_propery_date_view_and_click_on_go_button() {
+	public void select_the_group_propery_date_view_and_click_on_go_button() throws InterruptedException {
 		pnlMonthly.selectParameters();
 	}
 	
 	@Then("Page should load the defualt static section")
-	public void page_should_load_the_defualt_static_section(io.cucumber.datatable.DataTable dataTable) {
+	public void page_should_load_the_defualt_static_section() {
 		assertTrue(pnlMonthly.verifyStaticSection());
+	}
+	
+	@When("ROOMS SOLD divided by ROOMS AVAILABLE")
+	public void rooms_sold_divided_by_rooms_available() {
+		pnlMonthly.occupancyCalFunc();
+	}
+
+	@Then("Calculated value should be match with OCCUPANCY")
+	public void calculated_value_should_be_match_with_occupancy() {
+		assertTrue(pnlMonthly.verifyOccCalculationFunc());
+	}
+	
+	@When("Total Rooms Revenue divided by ROOMS SOLD")
+	public void total_rooms_revenue_divided_by_rooms_sold() {
+		pnlMonthly.adrCalFunc();
+	}
+
+	@Then("Calculated value should be match with ADR")
+	public void calculated_value_should_be_match_with_adr() {
+		assertTrue(pnlMonthly.verifyAdrCalculationFunc());
+	}
+	
+	@When("Total Rooms Revenue divided by ROOMS AVAILABLE")
+	public void total_rooms_revenue_divided_by_rooms_available() {
+		pnlMonthly.revParCalFunc();
+	}
+
+	@Then("Calculated value should be match with REV-PAR")
+	public void calculated_value_should_be_match_with_rev_par() {
+		assertTrue(pnlMonthly.verifyRevParCalculationFunc());
+	}
+	
+	@When("TOTAL OPERATING REVENUE divided by ROOMS AVAILABLE")
+	public void total_operating_revenue_divided_by_rooms_available() {
+		pnlMonthly.totalRevParCalFunc();
+	}
+
+	@Then("Calculated value should be match with TOTAL REV-PAR")
+	public void calculated_value_should_be_match_with_total_rev_par() {
+		assertTrue(pnlMonthly.verifyTotalRevParCalculationFunc());
+	}
+
+	@Then("the P&L should show the relevent sections that belong to the Owner's view")
+	public void the_p_l_should_show_the_relevent_sections_that_belong_to_the_owner_s_view() {
+		assertTrue(pnlMonthly.verifyOwnersViewSection());
+	}
+
+	@When("user selects the Operators View")
+	public void user_selects_the_operators_view() {
+		pnlMonthly.selectOperatorView();   
+	}
+
+	@Then("the P&L should show the relevent sections that belong to the Operators View")
+	public void the_p_l_should_show_the_relevent_sections_that_belong_to_the_Operators_view() {
+		assertTrue(pnlMonthly.verifyOperatorSection());
+	}
+	
+	@When("user selects the Room Revenue Detail View")
+	public void user_selects_the_room_revenue_detail_view() {
+		pnlMonthly.selectRooRevenueDetailView();
+	}
+
+	@Then("the P&L should show the relevent sections that belong to the Room Revenue Detail view")
+	public void the_p_l_should_show_the_relevent_sections_that_belong_to_the_room_revenue_detail_view() {
+		assertTrue(pnlMonthly.verifyRoomRevenueDetailSection());
 	}
 
 }
--- mdo_automation/src/test/resources/features/myP2_Features/Reports/P&L Monthly Report.feature
+++ mdo_automation/src/test/resources/features/myP2_Features/Reports/P&L Monthly Report.feature
@@ -2,15 +2,43 @@
 Feature: P&L Monthly Report
 
 
-Background: navigate to P&L report
+
+Background: Navigate to P&L Monthly report
     Given I am login to the myp2 site
     And System navigate to the home page
-       
- Scenario: Navigate to P&L Monthly and Click on GO button
-    When I am expand the P&L Statement option under Reports section in Side Menu
-    And go to the P&L Monthly page
+    And I am expand the P&L Statement option under Reports section in Side Menu
+    And go to the P&L Monthly page   
     And select the Group ,Propery, date,View and Click on GO button
+ 
+    Scenario: Verify the Static section 
     Then Page should load the defualt static section 
+ 
+    Scenario: Verify that the occupancy calculations are correct 
+    When ROOMS SOLD divided by ROOMS AVAILABLE
+    Then Calculated value should be match with OCCUPANCY
+    
+    Scenario: Verify that the ADR calculations are correct 
+    When Total Rooms Revenue divided by ROOMS SOLD
+    Then Calculated value should be match with ADR
+    
+    Scenario: Verify that the REV-PAR calculations are correct 
+    When Total Rooms Revenue divided by ROOMS AVAILABLE
+    Then Calculated value should be match with REV-PAR
+    
+    Scenario: Verify that the TOTAL REV-PAR calculations are correct 
+    When TOTAL OPERATING REVENUE divided by ROOMS AVAILABLE
+    Then Calculated value should be match with TOTAL REV-PAR
+    
+    Scenario: Verify whether users should be allowed to choose Owners view from a dropdown menu.
+    Then the P&L should show the relevent sections that belong to the Owner's view 
+    
+    Scenario: Verify whether users should be allowed to choose Operators view from a dropdown menu.
+    When user selects the Operators View
+    Then the P&L should show the relevent sections that belong to the Operators View
+    
+    Scenario: Verify whether users should be allowed to choose Room Revenue Detail view from a dropdown menu.
+    When user selects the Room Revenue Detail View
+    Then the P&L should show the relevent sections that belong to the Room Revenue Detail view
       
 
      

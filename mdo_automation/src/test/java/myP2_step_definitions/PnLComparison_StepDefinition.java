9f656ce2462bbb37bf1e6adf0ff0dfeb8f59480d
sannorj-mdosl <89373152+sannorj-mdosl@users.noreply.github.com>
2022-04-18 09:27:38 +0530

    P&L comparison

---
+++ mdo_automation/src/test/java/myP2_step_definitions/PnLComparison_StepDefinition.java
@@ -0,0 +1,24 @@
+package myP2_step_definitions;
+
+import static org.junit.Assert.assertTrue;
+
+import factory.DriverFactory;
+import io.cucumber.java.en.Given;
+import myP2_pageObjects.PnLComparison_PageObject;
+
+
+public class PnLComparison_StepDefinition {
+	
+	private PnLComparison_PageObject pnlComparison = new PnLComparison_PageObject(DriverFactory.getDriver());
+	
+	@Given("go to the P&L Property Comparison")
+	public void go_to_the_p_l_property_comparison() {
+	    assertTrue(pnlComparison.navigatePnLComparison());
+	}
+	
+	@Given("Select the Group , date,View and Click on GO button")
+	public void select_the_group_date_view_and_click_on_go_button() throws InterruptedException {
+		pnlComparison.selectParameters();
+	}
+
+}

a07ffdc9ed1ae1cea0f23614c8da57a86bac00de
sannorj-mdosl <89373152+sannorj-mdosl@users.noreply.github.com>
2022-05-30 10:00:32 +0530

    Guest Ledger

---
+++ mdo_automation/src/main/java/myP2_pageObjects/Guest_Ledger_PageObject.java
@@ -0,0 +1,623 @@
+package myP2_pageObjects;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import utils.ConstantsReader;
+import utils.ElementUtils;
+
+public class Guest_Ledger_PageObject {
+	
+	String ColumnData[][] ;
+	private WebDriver driver;
+	private ConstantsReader configReader = new ConstantsReader();
+
+	public Guest_Ledger_PageObject(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//button[@data-el='menuToggle']")
+	WebElement mainMenuButton;
+	
+	@FindBy(xpath = "//div[text()='Guest Ledger']//ancestor::li")
+	WebElement menuGuestLedger;
+	
+	@FindBy(xpath = "//h1[text()='Guest Ledger']")
+	WebElement h1GuestLedger;
+	
+	@FindBy(xpath = "(//div/input[contains(@class, 'MuiInputBase-inputAdornedEnd')])[1]")
+	WebElement drpOrg;
+	
+	@FindBy(xpath = "//div/input[contains(@name, 'hotelGroupId')]")
+	WebElement drpGroup;
+	
+	@FindBy(xpath = "//ul[@role='listbox']//li")
+	List <WebElement> listDrpValueSize;
+	
+	@FindBy(xpath = "//span[text()='MARSHA-Cd']")
+	WebElement lblMARSHA;
+	
+	@FindBy(xpath = "//button[@data-el='buttonOpenFilters']")
+	WebElement btnFilter;
+	
+	@FindBy(xpath = "//button[@data-el='buttonResetFilters']")
+	WebElement btnReset;
+	
+	@FindBy(xpath = "//div[text()='Filters']")
+	WebElement lblFilters;
+	
+	@FindBy(xpath = "//span[text()='Filters']")
+	WebElement lblbtnMainfilter;
+	
+	@FindBy(xpath = "//input[@name='hotelCode']")
+	WebElement drpMARSHA;
+	
+	@FindBy(xpath = "//input[@name='roomType']")
+	WebElement drpType;
+	
+	@FindBy(xpath = "//input[@name='groupCode']/../div")
+	WebElement drpFilterGroup;
+	
+	@FindBy(xpath = "//input[@name='settlementCode']/../div")
+	WebElement drpFilterDB;
+	
+	@FindBy(xpath = "//input[@name='settlementType']/../div")
+	WebElement drpFilterSettlementType;
+	
+	@FindBy(xpath = "//input[@name='folio']/../div")
+	WebElement drpFilterFolio;
+	
+	@FindBy(xpath = "//input[@name='arrivalDate']")
+	WebElement drpFilterArrivalDate;
+	
+	@FindBy(xpath = "//input[@name='departureDate']")
+	WebElement drpFilterDepartureDate;
+	
+	@FindBy(xpath = "//input[@name='numberOfNight']/../div")
+	WebElement drpFilterNONight;
+	
+	@FindBy(xpath = "//input[@name='outstandingAmountOperator']/../div")
+	WebElement drpOutstandingAmount;
+	
+	@FindBy(xpath = "//input[@name='outstandingAmount']")
+	WebElement txtOutstandingAmount;
+	
+	@FindBy(xpath = "//input[@name='authorizedLimitOperator']/../div")
+	WebElement drpLimit;
+	
+	@FindBy(xpath = "//input[@name='authorizedLimitAmount']")
+	WebElement txtlimit;
+	
+	@FindBy(xpath = "//button[@data-el='buttonSave']")
+	WebElement btnApply;
+
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[1]/div/div/div/div/div")
+	List <WebElement> listMARSHARows;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[4]/div/div/div/div/div")
+	List <WebElement> listType;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[7]/div/div/div/div/div")
+	List <WebElement> listGroup;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[8]/div/div/div/div/div")
+	List <WebElement> listArrDt;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[9]/div/div/div/div/div")
+	List <WebElement> listNt;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[10]/div/div/div/div/div")
+	List <WebElement> listDepart;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[11]/div/div/div/div/div")
+	List <WebElement> listFolio;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[12]/div/div/div/div/div")
+	List <WebElement> listDB;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[13]/div/div/div/div/div")
+	List <WebElement> listST;
+
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[14]/div/div/div/div/div")
+	List <WebElement> listOutstdg;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr/td[15]/div/div/div/div/div")
+	List <WebElement> listLimit;
+	
+	@FindBy(xpath = "//input[@name='keyword']")
+	WebElement txtSearch;
+	
+	@FindBy(xpath = "//*[@id='root']/div[1]//table/tbody/tr")
+	List <WebElement> noOfRows;
+	
+	@FindBy(xpath = "//*[@id='root']/div[1]//table/tbody/tr/td/div/div/div/div/div")
+	List <WebElement> noOfCells;
+	
+	@FindBy(xpath = "//button[@data-el='buttonCancel']")
+	WebElement btnReset1;
+	
+	@FindBy(xpath = "//*[@id='root']//table/tbody/tr[1]/td/div/div/div/div/div")
+	List <WebElement> noOfColumns;
+	
+	
+	
+	public boolean navigateGuestLedgerFunc() throws InterruptedException {
+		
+		WebElement menu = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(mainMenuButton));
+		menu.click();
+		
+		/* mandatory pause */
+		Thread.sleep(1500);
+		
+		WebElement pnlStatementEle = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(menuGuestLedger));
+		pnlStatementEle.click();
+		
+		ElementUtils.waitForElementToDisplay(h1GuestLedger, 100);
+		return h1GuestLedger.isDisplayed();
+
+	}
+	
+	public void selectParametersFunc() throws InterruptedException {
+
+		int Org = driver.findElements(By.xpath("(//div/input[contains(@class, 'MuiInputBase-inputAdornedEnd')])[1]")).size();
+		if (Org > 0) {
+			/* Select the appropriate Group value from the drop-down menu. */
+			WebElement drpGroupEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpOrg));
+			drpGroupEle.click();
+
+			ExpectedConditions.visibilityOf(listDrpValueSize.get(0));
+			for (int i = 0; i < listDrpValueSize.size(); i++) {
+				if (listDrpValueSize.get(i).getText().equalsIgnoreCase(configReader.getProp("Ledger_Org"))) {
+					listDrpValueSize.get(i).click();
+				}
+			}
+		}
+
+		ElementUtils.waitForElementToDisplay(lblMARSHA, 100);
+
+		if (drpGroup.isEnabled()) {
+			/* Select the appropriate Group value from the drop-down menu. */
+			WebElement drpGroupEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpGroup));
+			drpGroupEle.click();
+
+			ExpectedConditions.visibilityOf(listDrpValueSize.get(0));
+			for (int i = 0; i < listDrpValueSize.size(); i++) {
+				if (listDrpValueSize.get(i).getText().equalsIgnoreCase(configReader.getProp("Ledger_Group"))) {
+					listDrpValueSize.get(i).click();
+				}
+			}
+		}
+
+		ElementUtils.waitForElementToDisplay(lblMARSHA, 100);
+		
+	}
+	
+	public void clickOnFilterFunc() throws InterruptedException {
+		
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+		
+	}
+	
+	
+	public void filterByMarshaCodeFunc() throws InterruptedException {
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpGroupEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpMARSHA));
+		drpGroupEle.click();
+
+		Thread.sleep(1500);
+		for (int i = 0; i < listDrpValueSize.size(); i++) {
+			if (listDrpValueSize.get(i).getText().equalsIgnoreCase(configReader.getProp("Ledger_MARSHA"))) {
+				listDrpValueSize.get(i).click();
+			}
+		}
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifyMarshadataFunc() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listMARSHARows.size(); x++) {
+			String value = listMARSHARows.get(x).getText();
+			if (value.equals(configReader.getProp("Ledger_MARSHA")) || value.equals("")) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+
+	public void filterByTypeFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpGroupEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpType));
+		drpGroupEle.click();
+
+		Thread.sleep(1500);
+		for (int i = 0; i < listDrpValueSize.size(); i++) {
+			if (listDrpValueSize.get(i).getText().equalsIgnoreCase(configReader.getProp("Ledger_Type"))) {
+				listDrpValueSize.get(i).click();
+			}
+		}
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifyTypedataFunc() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listType.size(); x++) {
+			String value = listType.get(x).getText();
+			if (value.equals(configReader.getProp("Ledger_Type")) || value.equals("")) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public void filterByGroupFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+		
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpGroupEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpFilterGroup));
+		drpGroupEle.click();
+		Thread.sleep(4000);
+		
+		for (int i = 0; i < listDrpValueSize.size(); i++) {
+			if (listDrpValueSize.get(i).getAttribute("data-value").equalsIgnoreCase(configReader.getProp("Ledger_Group"))) {	
+				listDrpValueSize.get(i).click();
+				Thread.sleep(1500);
+			}
+		}
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifyGroupdataFunc() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listGroup.size(); x++) {
+			String value = listGroup.get(x).getText();
+			if (value.equals(configReader.getProp("Ledger_Group")) || value.equals("")) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public void filterByDBCodeFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpDBEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpFilterDB));
+		drpDBEle.click();
+
+		Thread.sleep(2000);
+		for (int i = 0; i < listDrpValueSize.size(); i++) {
+			if (listDrpValueSize.get(i).getAttribute("data-value").equalsIgnoreCase(configReader.getProp("Ledger_DB"))) {
+				listDrpValueSize.get(i).click();
+			}
+		}
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifyDBCodedataFunc() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listDB.size(); x++) {
+			String value = listDB.get(x).getText();
+			if (value.equals(configReader.getProp("Ledger_DB")) || value.equals("")) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public void filterBySTFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpSTEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpFilterSettlementType));
+		drpSTEle.click();
+
+		Thread.sleep(1500);
+		for (int i = 0; i < listDrpValueSize.size(); i++) {
+			if (listDrpValueSize.get(i).getAttribute("data-value").equalsIgnoreCase(configReader.getProp("Ledger_ST"))) {
+				listDrpValueSize.get(i).click();
+			}
+		}
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifySTdataFunc() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listST.size(); x++) {
+			String value = listST.get(x).getText();
+			if (value.equals(configReader.getProp("Ledger_ST")) || value.equals("")) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public void filterByFolioFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpFolioEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpFilterFolio));
+		drpFolioEle.click();
+
+		Thread.sleep(1500);
+		for (int i = 0; i < listDrpValueSize.size(); i++) {
+			if (listDrpValueSize.get(i).getAttribute("data-value").equalsIgnoreCase(configReader.getProp("Ledger_Folio"))) {
+				listDrpValueSize.get(i).click();
+			}
+		}
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifyFolioDataFunc() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listFolio.size(); x++) {
+			String value = listFolio.get(x).getText();
+			if (value.equals(configReader.getProp("Ledger_Folio")) || value.equals("")) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public void filterByArrDateFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpFolioEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpFilterArrivalDate));
+		drpFolioEle.click();
+		
+		drpFilterArrivalDate.sendKeys(configReader.getProp("Ledger_ArrDate"));
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifyArrDateFunc() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listArrDt.size(); x++) {
+			String value = listArrDt.get(x).getText();
+			if (value.equals(configReader.getProp("Ledger_ArrDate")) || value.equals("")) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public void filterByDepDateFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpFolioEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpFilterDepartureDate));
+		drpFolioEle.click();
+
+		drpFilterDepartureDate.sendKeys(configReader.getProp("Ledger_Depdate")); 
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+
+	public boolean verifyDepDateFunc() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listDepart.size(); x++) {
+			String value = listDepart.get(x).getText();
+			if (value.equals(configReader.getProp("Ledger_Depdate")) || value.equals("")) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public void filterByOutStandGreaterFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpOutstandingEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpOutstandingAmount));
+		drpOutstandingEle.click();
+
+		Thread.sleep(4500);
+		for (int i = 0; i < listDrpValueSize.size(); i++) {
+			if (listDrpValueSize.get(i).getAttribute("data-value").equalsIgnoreCase(configReader.getProp("drpGreaterstanding"))) {
+				listDrpValueSize.get(i).click();
+			}
+		}
+		txtOutstandingAmount.click();
+		txtOutstandingAmount.sendKeys(configReader.getProp("Ledger_OutStanding"));
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifyOutStandGreater() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listOutstdg.size(); x++) {
+			double value = Double.parseDouble(listOutstdg.get(x).getText().replaceAll(",", "").replaceAll("\\$", ""));
+			double value2 = Double.parseDouble(configReader.getProp("Ledger_OutStanding"));
+			if (value == value2 || value > value2 || listOutstdg.get(x).getText() == "") {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}	
+	
+	public void filterByLimitAmountFunc() throws InterruptedException {
+
+		Thread.sleep(1500);
+		btnReset.click();
+		ElementUtils.waitForElementToDisplay(lblbtnMainfilter, 100);
+
+		btnFilter.click();
+		ElementUtils.waitForElementToDisplay(lblFilters, 100);
+
+		/* Select the appropriate Group value from the drop-down menu. */
+		WebElement drpLimitEle = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(drpLimit));
+		drpLimitEle.click();
+
+		Thread.sleep(4500);
+		for (int i = 0; i < listDrpValueSize.size(); i++) {
+			if (listDrpValueSize.get(i).getAttribute("data-value").equalsIgnoreCase(configReader.getProp("drplimit"))) {
+				listDrpValueSize.get(i).click();
+			}
+		}
+		txtlimit.click();
+		txtlimit.sendKeys(configReader.getProp("Ledger_Limit"));
+
+		btnApply.click();
+		ElementUtils.waitForElementToHide(lblFilters, 100);
+
+	}
+	
+	public boolean verifyLimitAmount() throws InterruptedException {
+		boolean flag = true;
+		for (int x = 0; x < listLimit.size(); x++) {
+			double value = Double.parseDouble(listLimit.get(x).getText().replaceAll(",", "").replaceAll("\\$", ""));
+			double value2 = Double.parseDouble(configReader.getProp("Ledger_Limit"));
+			if (value == value2 || value > value2 || listLimit.get(x).getText() == "") {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public void inputSearchParameterFunc() throws InterruptedException {
+		
+		ElementUtils.waitForElementToDisplay(lblMARSHA, 100);
+		txtSearch.sendKeys(configReader.getProp("Ledger_Search"));
+		txtSearch.sendKeys(Keys.BACK_SPACE);
+		Thread.sleep(4000);
+		
+	}
+	
+	public boolean verifySearchedParameterFunc() throws InterruptedException {
+
+		int columnCount;
+		boolean flag = true;
+		ColumnData = new String[listMARSHARows.size()][noOfColumns.size()];
+
+		for (int x = 0; x < listMARSHARows.size(); x++) {
+			for (int t = 0; t < noOfColumns.size(); t++) {
+				WebElement no = driver.findElement(By.xpath("//*[@id='root']//table/tbody/tr[" + (x + 1) + "]/td[" + (t + 1) + "]/div/div/div/div/div"));
+				ColumnData[x][t] = no.getText();
+
+			}
+		}
+
+		for (int x = 0; x < ColumnData.length; x++) {
+			columnCount = 0;
+			for (int t = 0; t < ColumnData[x].length; t++) {
+				if (ColumnData[x][t].equalsIgnoreCase("EVDP")) {
+					flag = true;
+					columnCount++;
+					break;
+				} else {
+					flag = false;
+				}
+			}
+		}
+		return flag;
+
+	}
+	
+	
+	
+}
---
+++ mdo_automation/src/test/java/myP2_step_definitions/GuestLedger_StepDefinition.java
@@ -0,0 +1,143 @@
+package myP2_step_definitions;
+
+import static org.junit.Assert.assertTrue;
+
+import factory.DriverFactory;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import myP2_pageObjects.Guest_Ledger_PageObject;
+
+
+public class GuestLedger_StepDefinition {
+	
+	private Guest_Ledger_PageObject GuestLedger = new Guest_Ledger_PageObject(DriverFactory.getDriver());
+	
+	@Given("go to the Guest Ledger")
+	public void go_to_the_guest_ledger() throws InterruptedException {
+	assertTrue(GuestLedger.navigateGuestLedgerFunc());
+	}
+
+	@Given("Select the Group")
+	public void select_the_group() throws InterruptedException {
+		GuestLedger.selectParametersFunc();
+	}
+
+	@When("User Click on  Filter option")
+	public void user_click_on_filter_option() throws InterruptedException {
+		GuestLedger.clickOnFilterFunc();
+	}
+
+	@When("User selects the Marsha Code from the filter options")
+	public void user_selects_the_marsha_code_from_the_filter_options() throws InterruptedException {
+		GuestLedger.filterByMarshaCodeFunc();
+	}
+
+	@Then("Records should be filtered based on the Marsha Code")
+	public void records_should_be_filtered_based_on_the_marsha_code() throws InterruptedException {
+	    assertTrue(GuestLedger.verifyMarshadataFunc());
+	}
+
+	@Then("User selects the Type from the filter options after clicking the reset button.")
+	public void user_selects_the_type_from_the_filter_options_after_clicking_the_reset_button() throws InterruptedException {
+		GuestLedger.filterByTypeFunc();
+	}
+
+	@Then("Records should be filtered based on the Type")
+	public void records_should_be_filtered_based_on_the_type() throws InterruptedException {
+		assertTrue(GuestLedger.verifyTypedataFunc());
+	}
+
+	@Then("User selects the Group from the filter options after clicking the reset button.")
+	public void user_selects_the_group_from_the_filter_options_after_clicking_the_reset_button() throws InterruptedException {
+		GuestLedger.filterByGroupFunc();
+	}
+
+	@Then("Records should be filtered based on the Group")
+	public void records_should_be_filtered_based_on_the_group() throws InterruptedException {
+		assertTrue(GuestLedger.verifyGroupdataFunc());
+	}
+
+	@Then("User selects the D\\/B Code from the filter options after clicking the reset button.")
+	public void user_selects_the_d_b_code_from_the_filter_options_after_clicking_the_reset_button() throws InterruptedException {
+		GuestLedger.filterByDBCodeFunc();
+	}
+
+	@Then("Records should be filtered based on the D\\/B Code")
+	public void records_should_be_filtered_based_on_the_d_b_code() throws InterruptedException {
+		assertTrue(GuestLedger.verifyDBCodedataFunc());
+	}
+
+	@Then("User selects the Settlement type from the filter options after clicking the reset button.")
+	public void user_selects_the_settlement_type_from_the_filter_options_after_clicking_the_reset_button() throws InterruptedException {
+		GuestLedger.filterBySTFunc();
+	}
+
+	@Then("Records should be filtered based on the Settlement type")
+	public void records_should_be_filtered_based_on_the_settlement_type() throws InterruptedException {
+		assertTrue(GuestLedger.verifySTdataFunc());
+	}
+
+	@Then("User selects the Folio from the filter options after clicking the reset button.")
+	public void user_selects_the_folio_from_the_filter_options_after_clicking_the_reset_button() throws InterruptedException {
+		GuestLedger.filterByFolioFunc();
+	}
+
+	@Then("Records should be filtered based on the Folio")
+	public void records_should_be_filtered_based_on_the_folio() throws InterruptedException {
+		assertTrue(GuestLedger.verifyFolioDataFunc());
+	}
+
+	@Then("User selects the Arrival date from the filter options after clicking the reset button.")
+	public void user_selects_the_arrival_date_from_the_filter_options_after_clicking_the_reset_button() throws InterruptedException {
+		GuestLedger.filterByArrDateFunc();
+	}
+
+	@Then("Records should be filtered based on the Arrival date")
+	public void records_should_be_filtered_based_on_the_arrival_date() throws InterruptedException {
+		assertTrue(GuestLedger.verifyArrDateFunc());
+	}
+
+	@Then("User selects the Departure date from the filter options after clicking the reset button.")
+	public void user_selects_the_departure_date_from_the_filter_options_after_clicking_the_reset_button() throws InterruptedException {
+		GuestLedger.filterByDepDateFunc();
+	}
+
+	@Then("Records should be filtered based on the Departure date")
+	public void records_should_be_filtered_based_on_the_departure_date() throws InterruptedException {
+		assertTrue(GuestLedger.verifyDepDateFunc());
+	}
+
+	@Then("User enters OutStanding amount")
+	public void user_enters_out_standing_amount() throws InterruptedException {
+		GuestLedger.filterByOutStandGreaterFunc();
+	}
+
+	@Then("Records should be filtered based on the OutStanding amount")
+	public void records_should_be_filtered_based_on_the_out_standing_amount() throws InterruptedException {
+		assertTrue(GuestLedger.verifyOutStandGreater());
+	}
+
+	@Then("User enters Limit amount from the filter options")
+	public void user_enters_limit_amount_from_the_filter_options() throws InterruptedException {
+		GuestLedger.filterByLimitAmountFunc();
+	}
+
+	@Then("Records should be filtered based on the Limit amount")
+	public void records_should_be_filtered_based_on_the_limit_amount() throws InterruptedException {
+		assertTrue(GuestLedger.verifyLimitAmount());
+	}
+	
+	@When("User inputs a parameter in the search field")
+	public void user_inputs_a_parameter_in_the_search_field() throws InterruptedException {
+		GuestLedger.inputSearchParameterFunc();
+	}
+
+	@Then("Table should load the results")
+	public void table_should_load_the_results() throws InterruptedException {
+		assertTrue(GuestLedger.verifySearchedParameterFunc());
+	}
+
+
+
+}

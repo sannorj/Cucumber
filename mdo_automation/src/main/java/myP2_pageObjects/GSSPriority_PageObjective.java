f26857fd47902719e65425fec6969d0b283dc9a4
MDO <MDO@DESKTOP-GB607G0.mshome.net>
2022-05-31 11:45:28 +0530

    GSS Priority FIX

--- mdo_automation/src/main/java/myP2_pageObjects/GSSPriority_PageObjective.java
+++ mdo_automation/src/main/java/myP2_pageObjects/GSSPriority_PageObjective.java
@@ -148,7 +148,7 @@ public class GSSPriority_PageObjective {
 	public boolean loadPriorityReport() throws InterruptedException {
 
 		btnGo.click();
-		Thread.sleep(10000);
+		Thread.sleep(15000);
 		if (reportData.size() > 0) {
 
 			return true;

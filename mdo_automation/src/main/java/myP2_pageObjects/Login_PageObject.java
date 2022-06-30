09c4f0f63b64b3a489d91d30580ce55ab88a4f9e
sannorj-mdosl <89373152+sannorj-mdosl@users.noreply.github.com>
2022-06-30 10:30:51 +0530

    increasing sleep

--- mdo_automation/src/main/java/myP2_pageObjects/Login_PageObject.java
+++ mdo_automation/src/main/java/myP2_pageObjects/Login_PageObject.java
@@ -62,7 +62,7 @@ public class Login_PageObject {
 			username.sendKeys(configReader.getProp("dev_userName"));
 			password.sendKeys(configReader.getProp("dev_password"));
 			passwordReset.isDisplayed();
-			Thread.sleep(10000); 
+			Thread.sleep(15000); 
 			loginButton.click();
 
 		} else if (myEnv.toLowerCase().equals("uat")) {

db4a88f73bc6c801c86061f4e0329496268ef857
HP <HP@DESKTOP-IEIVCFR>
2022-08-17 11:59:11 +0530

    PNL ttm forcast validation script

---
+++ mdo_automation/src/main/java/myP2_pageObjects/TTMForcast_PageObject.java
@@ -0,0 +1,176 @@
+package myP2_pageObjects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utils.ConfigReader;
+import utils.ConstantsReader;
+
+public class TTMForcast_PageObject {
+
+	private WebDriver driver;
+	private ConstantsReader configReader = new ConstantsReader();
+	ArrayList<Double> lstForcast, lstTTMForcast;
+
+	public TTMForcast_PageObject(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		lstForcast = new ArrayList<>();
+		lstTTMForcast = new ArrayList<>();
+	}
+
+	@FindBy(xpath = "//button[@data-el='menuToggle']")
+	WebElement mainMenuButton;
+
+	@FindBy(xpath = "//div[text()='Reports']//ancestor::li")
+	WebElement reports;
+
+	@FindBy(xpath = "//div[text()='P&L Statements']//ancestor::li")
+	WebElement pnlStatement;
+
+	@FindBy(xpath = "//div[text()='P&L Yearly']//ancestor::li")
+	WebElement pnlYearly;
+
+	@FindBy(xpath = "//h1[text()='Profit & Loss Yearly Report']")
+	WebElement pnlYearlyPage;
+
+	@FindBy(xpath = "//input[@name='portfolio-group']")
+	WebElement dropDownGroup;
+
+	@FindBy(xpath = "//ul[@role='listbox']//li")
+	List<WebElement> lstDropDownGroup;
+
+	@FindBy(xpath = "//input[@name='portfolio-hotel']")
+	WebElement dropDownHotel;
+
+	@FindBy(xpath = "//ul[@role='listbox']//li")
+	List<WebElement> lstDropDownHotel;
+
+	@FindBy(xpath = "//div[@id='mui-component-select-selectedYear']")
+	WebElement dropDownPeriod;
+
+	@FindBy(xpath = "//ul[@role='listbox']//li")
+	List<WebElement> lstDropDownPeriod;
+
+	@FindBy(xpath = "//div[@id='mui-component-select-customViewId']")
+	WebElement dropDownView;
+
+	@FindBy(xpath = "//ul[@role='listbox']//li")
+	List<WebElement> lstDropDownView;
+
+	@FindBy(xpath = "//button[@data-el='buttonGo']")
+	WebElement btnGo;
+
+	@FindBy(xpath = "//div[text()='Rooms available']")
+	WebElement lblRoomAva;
+
+	@FindBy(xpath = "//th//span[contains(text(),'TTM Actual')]")
+	WebElement ttmHeader;
+
+	@FindBy(xpath = "//h3[text()='Edit Columns']")
+	WebElement lblEdit;
+
+	@FindBy(xpath = "//button[@data-el='buttonFilter']")
+	WebElement btnFilter;
+
+	@FindBy(xpath = "//tr[@class='MuiTableRow-root MuiTableRow-head']//th[@freezecolumns='0']")
+	List<WebElement> lstReportMonthHeader;
+
+	@FindBy(xpath = "//tbody//tr[1]//td")
+	List<WebElement> lstForcastYear;
+
+	@FindBy(xpath = "//tbody//tr[1]//td")
+	List<WebElement> lstForcastTTM;
+
+	public void passParameteres() {
+
+		try {
+			dropDownGroup.click();
+			Thread.sleep(1500);
+			for (int i = 0; i < lstDropDownGroup.size(); i++) {
+				if (lstDropDownGroup.get(i).getText().equalsIgnoreCase(configReader.getProp("TTMGroup"))) {
+					lstDropDownGroup.get(i).click();
+
+				}
+			}
+
+			dropDownHotel.click();
+			Thread.sleep(1500);
+			for (int i = 0; i < lstDropDownHotel.size(); i++) {
+				if (lstDropDownHotel.get(i).getText().equalsIgnoreCase(configReader.getProp("TTMProperty"))) {
+					lstDropDownHotel.get(i).click();
+
+				}
+			}
+
+			dropDownPeriod.click();
+			Thread.sleep(2000);
+			lstDropDownPeriod.get(1).click();
+
+			dropDownView.click();
+			for (int i = 0; i < lstDropDownView.size(); i++) {
+				if (lstDropDownView.get(i).getText().equalsIgnoreCase(configReader.getProp("TTMView"))) {
+					lstDropDownView.get(i).click();
+
+				}
+			}
+
+		} catch (
+
+		Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void storeactualForcastData() {
+		for (int i = 0; i < 6; i++) {
+
+			WebElement forcastData = driver.findElement(By.xpath("//tbody//tr[" + (i + 1) + "]//td[" + 16 + "]"));
+
+			lstForcast.add(Double
+					.parseDouble(forcastData.getText().replaceAll(",", "").replaceAll("\\$", "").replaceAll("%", "")));
+
+		}
+		
+		for (int i = 0; i < 6; i++)
+		{
+			System.out.println("AAA"+lstForcast.get(i));
+		}
+	}
+
+	public void storeTTMForcastData() {
+		for (int i = 0; i < 6; i++) {
+
+			WebElement forcastData = driver.findElement(By.xpath("//tbody//tr[" + (i + 1) + "]//td[" + 16 + "]"));
+
+			lstTTMForcast.add(Double
+					.parseDouble(forcastData.getText().replaceAll(",", "").replaceAll("\\$", "").replaceAll("%", "")));
+
+		}
+		for (int i = 0; i < 6; i++)
+		{
+			System.out.println("BBB"+lstTTMForcast.get(i));
+		}
+	}
+
+	public boolean compareForcastData() {
+		boolean flag = false;
+
+		for (int i = 0; i < lstTTMForcast.size(); i++) {
+
+			if (lstTTMForcast.get(i) == lstTTMForcast.get(i)) {
+				flag = true;
+			} else {
+				flag = false;
+				break;
+			}
+		}
+		return flag;
+	}
+}
---
+++ mdo_automation/src/test/resources/features/myP2_Features/Reports/TTMForcast.feature
@@ -0,0 +1,35 @@
+#Author: haniffa@mydigitaloffice.ca
+@myP2_Regression
+
+Feature: P&L Yearly - TTM Forcast Functionalty 
+
+	 Background: Navigate to P&L report Func
+   Given I am login to the myp2 site
+   And System navigate to the home page
+   And Select the organization as "Beck Legacy Group" 
+   And I am expand the P&L Statement option under Reports section in Side Menu
+   Then I am navigate to P&L Yearly page
+   
+   Scenario: Verify PnL Yearly Static Section
+   Given I select the group,property,view
+   And I am Loading the PnLYearly Report with GO button
+   Then Page should load the defualt static section
+   
+   Scenario: Verify whethere Page load according to TTM Year function
+   Given I select the group,property,view
+   When I am Loading the PnLYearly Report with GO button
+   Then Page should load the defualt static section
+   And  Verify whether the header load according to TTM selected
+   
+    Scenario: Storing and comparing actual forcast data with TTM forcast data
+    Given I select the group,property,view in TTM
+    When I am Loading the PnLYearly Report with GO button
+    Then Page should load the defualt static section
+    And I am storing the TTM forcast data
+    Given I select the group,property,view
+    When I am Loading the PnLYearly Report with GO button
+    Then Page should load the defualt static section
+    And I am storing the actual forcast data
+    And I compare the stored forcast data
+    
+    

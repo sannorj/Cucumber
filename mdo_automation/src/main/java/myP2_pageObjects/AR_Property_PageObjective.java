e70b25160ac79d555cf53b79c6db810c026015f7
HP <HP@DESKTOP-IEIVCFR>
2022-08-30 18:56:57 +0530

    ARPropertyFIX

--- mdo_automation/src/main/java/myP2_pageObjects/AR_Property_PageObjective.java
+++ mdo_automation/src/main/java/myP2_pageObjects/AR_Property_PageObjective.java
@@ -103,7 +103,7 @@ public class AR_Property_PageObjective {
 
 		btnGo.click();
 
-		WebElement txtProperty = new WebDriverWait(driver, Duration.ofSeconds(40))
+		WebElement txtProperty = new WebDriverWait(driver, Duration.ofSeconds(100))
 				.until(ExpectedConditions.visibilityOf(txtRowField));
 
 		return txtProperty.isDisplayed();

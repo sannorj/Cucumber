9b45f2132130ed1e4c3c0a76c38c14fa543533f7
Sannorj <Sannorj@DESKTOP-CGKD4M7.mshome.net>
2022-12-23 05:20:15 +0530

    P&L Monthly Report - Edit Column functionality   fix

--- mdo_automation/src/main/java/myP2_pageObjects/PnLMonthly_PageObject.java
+++ mdo_automation/src/main/java/myP2_pageObjects/PnLMonthly_PageObject.java
@@ -175,7 +175,8 @@ public class PnLMonthly_PageObject {
 		Thread.sleep(1500);
 		WebElement drpViewEle = new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(drpView));
 		drpViewEle.click();
-		Thread.sleep(4500);
+		
+		Thread.sleep(6500);
 
 		for (int i = 0; i < listDrpValueSize.size(); i++) {
 			if (listDrpValueSize.get(i).getText().equalsIgnoreCase(configReader.getProp("View"))) {

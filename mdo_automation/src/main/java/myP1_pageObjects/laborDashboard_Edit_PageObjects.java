2bb660dced01d2bcdf62cc2d0b5ad6e1a76bc53f
Admin <Admin@DESKTOP-IF089PD>
2023-05-09 13:10:08 +0530

    labor error fix

--- mdo_automation/src/main/java/myP1_pageObjects/laborDashboard_Edit_PageObjects.java
+++ mdo_automation/src/main/java/myP1_pageObjects/laborDashboard_Edit_PageObjects.java
@@ -31,9 +31,14 @@ public class laborDashboard_Edit_PageObjects {
 	
 	public void desableCard() throws InterruptedException {
 		editBtn.click();
-		WebElement addChartBtn = new WebDriverWait(driver, Duration.ofSeconds(100))
+		WebElement addChartBtn = new WebDriverWait(driver, Duration.ofSeconds(900))
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='btnAddChart']")));
 		addChartBtn.click();
+
+		Thread.sleep(3000);
+		WebElement UserNameLoad = new WebDriverWait(driver, Duration.ofSeconds(900))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[contains(text(),'User Name')]//parent::div//a[@class='select2-choice']//span[text()='"+configReader.getMYP1Prop("Labor_UserName")+"']")));
+		
 		Thread.sleep(2000);
 		WebElement editLRSettingsModal = new WebDriverWait(driver, Duration.ofSeconds(700)).until(ExpectedConditions
 				.visibilityOfElementLocated(By.xpath("//h4[text()='Add Chart']")));
@@ -41,23 +46,35 @@ public class laborDashboard_Edit_PageObjects {
 		WebElement offColumn = driver.findElement(By.xpath("//label[text()='"+configReader.getMYP1Prop("Labor_Edit_AddChart")+"']//following-sibling::div//div[@class='ios-switch on']"));
 		offColumn.click();
 		System.out.println("disabled column== "+configReader.getMYP1Prop("Labor_Edit_AddChart"));
-		
 		try {
 			JavascriptExecutor executor = (JavascriptExecutor) driver;
 			executor.executeScript("arguments[0].click();", submitBtn);
+			System.out.println("submit btn click");
 		} catch (Exception e) {
 			JavascriptExecutor executor = (JavascriptExecutor) driver;
 			executor.executeScript("arguments[0].click();", submitBtn);
+			System.out.println("submit btn clickkkkkkkkk");
 		}
 		Thread.sleep(3000);
 	}
 
 	public boolean verifyCardDesable() throws InterruptedException {
-		Thread.sleep(6000);
+		Thread.sleep(9000);
+		try {
+			Boolean invisibleCloseIcon = new WebDriverWait(driver, Duration.ofSeconds(900)).until(ExpectedConditions
+				.invisibilityOfElementLocated(By.xpath("//header//div[@class='panel-actions']//a[@title='Close']")));
+		
+		} catch (NoSuchElementException e) {
+			Boolean invisibleCloseIcon = new WebDriverWait(driver, Duration.ofSeconds(900)).until(ExpectedConditions
+				.invisibilityOfElementLocated(By.xpath("//header//div[@class='panel-actions']//a[@title='Close']")));
+		}
+		System.out.println("close icon invisibled");
+		
 		WebElement tblLoad = new WebDriverWait(driver, Duration.ofSeconds(900)).until(ExpectedConditions
 				.visibilityOfElementLocated(By.xpath("//button[@title='Edit']")));
+		System.out.println("edit button visible");
 		try {
-				int findCard = driver.findElements(By.xpath("//label[text()='"+configReader.getMYP1Prop("Labor_Edit_CloseCard")+"']")).size();
+				int findCard = driver.findElements(By.xpath("//h2[text()='"+configReader.getMYP1Prop("Labor_Edit_CloseCard")+"']")).size();
 				if(findCard>0) {
 					System.out.println(configReader.getMYP1Prop("Labor_Edit_AddChart")+" is not disabled!");
 					return false;
@@ -71,18 +88,36 @@ public class laborDashboard_Edit_PageObjects {
 
 	public void enableCard() throws InterruptedException {
 		editBtn.click();
-		WebElement addChartBtn = new WebDriverWait(driver, Duration.ofSeconds(100))
+		Thread.sleep(3000);
+		System.out.println("edit button click");
+		WebElement addChartBtn = new WebDriverWait(driver, Duration.ofSeconds(500))
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='btnAddChart']")));
 		addChartBtn.click();
+		System.out.println("add chart btn click");
 		Thread.sleep(2000);
 		WebElement editLRSettingsModal = new WebDriverWait(driver, Duration.ofSeconds(700)).until(ExpectedConditions
 				.visibilityOfElementLocated(By.xpath("//h4[text()='Add Chart']")));
+		
+		WebElement UserNameLoad = new WebDriverWait(driver, Duration.ofSeconds(900))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[contains(text(),'User Name')]//parent::div//a[@class='select2-choice']//span[text()='"+configReader.getMYP1Prop("Labor_UserName")+"']")));
+		
+		Thread.sleep(2000);
+		System.out.println("add chart view");
 		
 		WebElement firstColValue = driver.findElement(By.xpath("//label[text()='"+configReader.getMYP1Prop("Labor_Edit_AddChart")+"']//following-sibling::div//div[@class=@class='ios-switch off']"));
 		firstColValue.click();
 		System.out.println("disabled column== "+configReader.getMYP1Prop("Labor_Edit_AddChart"));
 		Thread.sleep(1000);
-		submitBtn.click();
+//		submitBtn.click();
+		try {
+			JavascriptExecutor executor = (JavascriptExecutor) driver;
+			executor.executeScript("arguments[0].click();", submitBtn);
+			System.out.println("submit btn clickss");
+		} catch (Exception e) {
+			JavascriptExecutor executor = (JavascriptExecutor) driver;
+			executor.executeScript("arguments[0].click();", submitBtn);
+			System.out.println("submit btn clickkkkkkkkksss");
+		}
 		Thread.sleep(3000);
 	}
 
@@ -90,8 +125,10 @@ public class laborDashboard_Edit_PageObjects {
 		Thread.sleep(6000);
 		WebElement tblLoad = new WebDriverWait(driver, Duration.ofSeconds(700)).until(ExpectedConditions
 				.visibilityOfElementLocated(By.xpath("//button[@title='Edit']")));
+		Thread.sleep(3000);
 		try {
-				int findCard = driver.findElements(By.xpath("//label[text()='"+configReader.getMYP1Prop("Labor_Edit_CloseCard")+"']")).size();
+			System.out.println("#####################");
+				int findCard = driver.findElements(By.xpath("//h2[text()='"+configReader.getMYP1Prop("Labor_Edit_CloseCard")+"']")).size();
 				if(findCard>0) {
 					System.out.println(configReader.getMYP1Prop("Labor_Edit_CloseCard")+" is available!");
 					return true;
@@ -99,7 +136,7 @@ public class laborDashboard_Edit_PageObjects {
 			} catch (NoSuchElementException e) {
 				System.out.println("NoSuchElementException occoured== "+configReader.getMYP1Prop("Labor_Edit_CloseCard"));
 			}
-			
+		System.out.println(configReader.getMYP1Prop("Labor_Edit_CloseCard")+" is not available!");
 		return false;
 	}
 

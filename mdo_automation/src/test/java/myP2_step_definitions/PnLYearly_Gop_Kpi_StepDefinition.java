f9371ab4c827317350add365d35774735606f212
pasindu-mdosl <pasindu@mydigitaloffice.ca>
2023-06-16 17:52:41 +0530

    Gop Kpi code optimize

---
+++ mdo_automation/src/test/java/myP2_step_definitions/PnLYearly_Gop_Kpi_StepDefinition.java
@@ -0,0 +1,46 @@
+package myP2_step_definitions;
+
+import static org.junit.Assert.assertTrue;
+
+import factory.DriverFactory;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import myP2_pageObjects.PnLYearly_Gop_Kpi_PageObject;
+
+public class PnLYearly_Gop_Kpi_StepDefinition {
+
+	private PnLYearly_Gop_Kpi_PageObject pnlYearlyGopKpi = new PnLYearly_Gop_Kpi_PageObject(DriverFactory.getDriver());
+
+	@Then("I clicks GO button and confirm if the sections, GOP Flow Thru Prior Year Actuals & GOP Margin sections are visible in the P&L Yearly report")
+	public void I_clicks_GO_button_and_confirm_if_the_sections_gop_flow_thru_prior_year_actuals_gop_margin_sections_are_visible_in_the_p_l_yearly_report() throws InterruptedException {
+		pnlYearlyGopKpi.verifyGopKpi();
+	}
+
+	@Then("I select the group {string} , property {string} , specific date , view {string} and confirm GOP KPI View section is not visible in view dropdown")
+	public void i_select_the_group_property_specific_date_view_and_confirm_gop_kpi_view_section_is_not_visible_in_view_dropdown(String group, String property, String view) throws InterruptedException {
+		assertTrue(pnlYearlyGopKpi.verifyParametersAndGopNotVisi(group, property, view));
+	}
+
+	@Then("I clicks GO button")
+	public void i_clicks_go_button() throws InterruptedException {
+		pnlYearlyGopKpi.clickGoButt();
+	}
+
+	@Then("confirm if the sections, GOP Flow Thru Prior Year Actuals & GOP Margin sections are not visible in the PnL Monthly report")
+	public void confirm_if_the_sections_gop_flow_thru_prior_year_actuals_gop_margin_sections_are_not_visible_in_the_PnL_Monthly_report() throws InterruptedException {
+		assertTrue(pnlYearlyGopKpi.verifyPnlMheader());
+	}
+	
+	@Then("I select the group {string} , specific date , view {string} and confirm GOP KPI View section is not visible in view dropdown")
+	public void i_select_the_group_specific_date_view_and_confirm_gop_kpi_view_section_is_not_visible_in_view_dropdown(String Group, String View) throws InterruptedException {
+		assertTrue(pnlYearlyGopKpi.verifyParameterAndGopNotVisible(Group, View));
+	}
+	
+	@Then("confirm if the sections, GOP Flow Thru Prior Year Actuals & GOP Margin sections are not visible in the PnL Property Comparison report")
+	public void confirm_if_the_sections_gop_flow_thru_prior_year_actuals_gop_margin_sections_are_not_visible_in_the_PnL_Property_Comparison_report() throws InterruptedException {
+		assertTrue(pnlYearlyGopKpi.verifyPnlPCheader());
+	}
+	
+}
---
+++ mdo_automation/src/test/resources/features/myP2_Features/Reports/P&L Yearly Gop Kpi.feature
@@ -0,0 +1,28 @@
+#Author: haniffa@mydigitaloffice.ca
+@myP2_Regression  @myP2_Smoke @PnLYearly
+
+
+Feature: P&L Yearly - GOP KPI are not displayed for P&L Monthly & P&L Comparison reports
+
+  Background: Navigate to P&L Yearly page and verify functionality  
+    Given I am login to the myp2 site
+    And System navigate to the home page
+    And Select the organization as "Insignia Hospitality Group" 
+    And I am expand the P&L Statement option under Reports section in Side Menu
+    
+    Scenario: Verify the P&L Yearly exclusive sections are not displayed in P&L Monthly and P&L Property Comparison report
+    Then I am navigate to P&L Yearly page
+		When I select the group "1Insignia Hospitality Group" , property "CNMTS" , year "2020" , view "GOP KPI View"
+		Then I clicks GO button and confirm if the sections, GOP Flow Thru Prior Year Actuals & GOP Margin sections are visible in the P&L Yearly report
+		And I am expand the P&L Statement option under Reports section in Side Menu
+		And go to the P&L Monthly page 
+		And I select the group "1Insignia Hospitality Group" , property "CNMTS" , specific date , view "Owner's View" and confirm GOP KPI View section is not visible in view dropdown
+		And I clicks GO button
+		And confirm if the sections, GOP Flow Thru Prior Year Actuals & GOP Margin sections are not visible in the PnL Monthly report
+		
+		
+		Given I am expand the P&L Statement option under Reports section in Side Menu
+		And go to the P&L Property Comparison
+		Then I select the group "1Insignia Hospitality Group" , specific date , view "Owner's View" and confirm GOP KPI View section is not visible in view dropdown
+		And I clicks GO button
+		And confirm if the sections, GOP Flow Thru Prior Year Actuals & GOP Margin sections are not visible in the PnL Property Comparison report
